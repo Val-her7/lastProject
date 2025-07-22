6eb21c03483f118a934dcf898d9107ca37dcb08d
Valher7 <Valentin.herman.pro@gmail.com>
2025-07-22 13:43:27 +0200

    Initial commit

---
+++ apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,6 @@
+import { faHome, faUser} from '@fortawesome/free-solid-svg-icons';
+import { IconDefinition } from '@fortawesome/angular-fontawesome';
+export const fontAwesomeIcons: IconDefinition[] = [
+    faUser,
+    faHome,
+]
