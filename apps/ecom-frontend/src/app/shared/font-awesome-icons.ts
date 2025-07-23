bf1d6b8f2d4166c90add3dcb85735c782b6a2c83
Valher7 <Valentin.herman.pro@gmail.com>
2025-07-23 11:05:26 +0200

    feat: add footer structure

--- apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
+++ apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
@@ -1,6 +1,11 @@
-import { faCartShopping, faUser} from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping, faTruckFast, faUser} from '@fortawesome/free-solid-svg-icons';
 import { IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 export const fontAwesomeIcons: IconDefinition[] = [
     faUser,
     faCartShopping,
+    faTruckFast,
+    faFacebook,
+    faInstagram
 ]
