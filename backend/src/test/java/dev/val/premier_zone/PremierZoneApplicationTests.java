203c2cd609c7d7ead2edce5ab2ca8a1efc44e344
Valher7 <Valentin.herman.pro@gmail.com>
2025-08-04 16:04:26 +0200

    add spring boot initializer

---
+++ backend/src/test/java/dev/val/premier_zone/PremierZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.val.premier_zone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
