55e212eda38fe3537018c4d47b96178006a1abb7
Valher7 <Valentin.herman.pro@gmail.com>
2025-08-05 16:46:22 +0200

    feat: add Player model

---
+++ backend/src/main/java/dev/val/premier_zone/model/Player.java
@@ -0,0 +1,35 @@
+package dev.val.premier_zone.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "player_stats")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Player {
+
+    @Id
+    @Column(name = "player", unique = true)
+    private String name;
+    private String nation;
+    private String pos;
+    private Integer age;
+    private Integer mp;
+    private Integer starts;
+    private Double min;
+    private Double gls;
+    private Double ast;
+    private Double pk;
+    private Double crdy;
+    private Double crdr;
+    private Double xg;
+    private Double xag;
+    private String team;
+}
