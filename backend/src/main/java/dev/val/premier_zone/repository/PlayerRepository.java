f5f9e263400a34d537f86eba11abc1a0ca039acc
Valher7 <Valentin.herman.pro@gmail.com>
2025-08-05 16:47:08 +0200

    feat: add PlayerRepository

---
+++ backend/src/main/java/dev/val/premier_zone/repository/PlayerRepository.java
@@ -0,0 +1,15 @@
+package dev.val.premier_zone.repository;
+
+import dev.val.premier_zone.model.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, String> {
+
+    void deleteByName(String name);
+
+    Optional<Player> findByName(String name);
+}
